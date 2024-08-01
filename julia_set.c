ab28c7e59ae134689474c0fae0f2b2523cba7fa6
Olena Hladkovska <ohladkov@student.42berlin.de>
2024-08-01 13:00:19 +0200

    refactor

--- julia_set.c
+++ julia_set.c
@@ -6,7 +6,7 @@
 /*   By: ohladkov <ohladkov@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/29 19:04:03 by ohladkov          #+#    #+#             */
-/*   Updated: 2023/11/26 17:27:04 by ohladkov         ###   ########.fr       */
+/*   Updated: 2024/08/01 12:33:49 by ohladkov         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -34,7 +34,6 @@ void	julia_calc(int x, int y, t_fractal *fractal)
 		fractal->itr++;
 	}
 	col = 255 * fractal->itr / fractal->max_itr;
-	// fractal->color = ft_rgb(col * fractal->c, col * fractal->c, col);
 	fractal->color = ft_rgb(col + fractal->c, col, col * fractal->c);
 	if (fractal->itr == fractal->max_itr)
 		ft_pixel_put(&fractal->img, x, y, 0x000000);
